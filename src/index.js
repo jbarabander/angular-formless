65bb91a491446dfa596ee5446924ee235e293901
Justin Barabander <jbarabander@gmail.com>
2016-12-30 12:10:32 -0500

    Some clean up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-var angular = require('angular');
 var directives = require('./directives');
-
 var app = angular.module('formless', []);
 
 app.directive('formless', directives.formless);
 
-module.exports = app;
+module.exports = 'formless';
